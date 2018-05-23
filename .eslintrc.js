05f56a1e45414d72602b845a4e019d753aa8948d
长卿 <changqing@dtstack.com>
2018-05-23 19:13:06 +0800

    加入eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-useless-escape": "off",
+        "no-console":"warn",
+        "no-undef":"off"
+    }
+}
